418b6379e6467d8d91aa6cf336c1a7ba4e0e7252
Ayushraj Pandey <99150585+ayushraj45@users.noreply.github.com>
2025-12-07 16:12:37 +0000

    Create SQL query for top 3 earners by department
    Added a SQL query to find the top 3 earners in each department using DENSE_RANK.

---
+++ plsql/find_top_three.sql
@@ -0,0 +1,13 @@
+--Write a SQL query to find the top 3 earners in each department. (Tables: Employee, Department).
+WITH RankedSalaries AS (
+    SELECT 
+        d.Name AS Department,
+        e.Name AS Employee,
+        e.Salary,
+        DENSE_RANK() OVER (PARTITION BY e.DepartmentId ORDER BY e.Salary DESC) as rank
+    FROM Employee e
+    JOIN Department d ON e.DepartmentId = d.Id
+)
+SELECT Department, Employee, Salary
+FROM RankedSalaries
+WHERE rank <= 3;
